b4f844e97b64c50d52046dc47085a15949a23207
Abdullah Al-jehani <abdallahaljehani926@gmail.com>
2024-12-07 15:59:34 +0200

    added the project section to the stories view

---
+++ src/components/Stories/ProjectsSectionComponent.vue
@@ -0,0 +1,167 @@
+<template>
+  <div class="grid grid-cols-12 w-full text-white place-content-center">
+    <div class="col-start-1 md:col-span-6 col-span-full">
+      <p class="font-light mb-1">Projects</p>
+      <h1 class="text-2xl xs:text-xl font-semibold mb-14">
+        Empowering Libya’s Next Generation of Innovators
+      </h1>
+      <div class="flex flex-col text-2xl leading-normal">
+        <div
+          v-for="(value, index) in values"
+          :key="index"
+          class="flex items-center gap-2 pb-8 cursor-pointer"
+          @mouseover="hoverValue(index)"
+          @mouseleave="clearHover"
+        >
+          <img
+            v-if="index === hoveredIndex"
+            src="/icons/Tatweer.svg"
+            alt="Tatweer Icon"
+            class="w-6 h-6"
+          />
+          <h1
+            :class="{
+              'font-bold text-white transition-all duration-300':
+                index === hoveredIndex,
+              'font-semibold text-greyColor transition-all duration-300':
+                index !== hoveredIndex,
+            }"
+          >
+            {{ value.name }}
+          </h1>
+        </div>
+      </div>
+    </div>
+    <div
+      class="md:col-start-9 col-start-1 col-span-full place-content-center flex md:flex-col flex-col-reverse"
+    >
+      <p
+        class="font-extralight leading-relaxed mb-4 transition-opacity duration-300"
+      >
+        {{ values[hoveredIndex]?.description || values[0].description }}
+      </p>
+      <div class="mb-8">
+        <ButtonComponent>Read More</ButtonComponent>
+      </div>
+
+      <div class="w-full overflow-hidden">
+        <transition
+          name="slide"
+          mode="out-in"
+          @before-enter="beforeEnter"
+          @enter="enter"
+          @after-enter="afterEnter"
+        >
+          <img
+            v-if="values[hoveredIndex]"
+            :src="values[hoveredIndex].image"
+            :alt="values[hoveredIndex].name"
+            :key="hoveredIndex"
+            class="w-full h-auto mb-4 md:mb-0"
+          />
+        </transition>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import ButtonComponent from "../../components/main/ButtonComponent.vue";
+const hoveredIndex = ref(0);
+const lastHoveredIndex = ref(0);
+
+const values = [
+  {
+    name: "Enjazi",
+    description:
+      "In 2017, Tatweer Research organized Enjazi, a competition for entrepreneurs, in collaborate with the MIT Enterprise Forum Pan Arab. After launching a call for applications with hundreds of startups joining from across Libya, Enjazi began as startups compeited over the course of three stages in three different cities. Tatweer Research held a launch ceremony for Enjazi on 8 April 2017, at its headquarters in Benghazi, followed by another ceremony in Tripoli on April 12, then another one on April 16 of the same year.",
+    image: "/images/Harmony.png",
+  },
+  {
+    name: "Tatweer Entrepruenuer Campus (TEC)",
+    description: "Approaching challenges with optimism and resilience.",
+    image: "/images/Positivity.png",
+  },
+  {
+    name: "TEC Talk",
+    description: "Prioritizing forward-thinking solutions for Libya's future.",
+    image: "/images/Innovation.png",
+  },
+  {
+    name: "TEC Camp",
+    description: "Embracing new ideas and diverse perspectives.",
+    image: "/images/OpenMindedness.png",
+  },
+  {
+    name: "TEC Crash",
+    description: "Developing the skills and talents of Libya’s people.",
+    image: "/images/InvestmentInHumanCapital.png",
+  },
+  {
+    name: "Benghazi Hackathon",
+    description: "Developing the skills and talents of Libya’s people.",
+    image: "/images/InvestmentInHumanCapital.png",
+  },
+  {
+    name: "Bedaya",
+    description: "Developing the skills and talents of Libya’s people.",
+    image: "/images/InvestmentInHumanCapital.png",
+  },
+  {
+    name: "Impact Fund",
+    description: "Developing the skills and talents of Libya’s people.",
+    image: "/images/InvestmentInHumanCapital.png",
+  },
+  {
+    name: "AgriTEC",
+    description: "Developing the skills and talents of Libya’s people.",
+    image: "/images/InvestmentInHumanCapital.png",
+  },
+  {
+    name: "TEC+ Business Accelerator",
+    description: "Developing the skills and talents of Libya’s people.",
+    image: "/images/InvestmentInHumanCapital.png",
+  },
+  {
+    name: "Public-Private Dialogues",
+    description: "Developing the skills and talents of Libya’s people.",
+    image: "/images/InvestmentInHumanCapital.png",
+  },
+  {
+    name: "Deraya",
+    description: "Developing the skills and talents of Libya’s people.",
+    image: "/images/InvestmentInHumanCapital.png",
+  },
+  {
+    name: "Al-Mreisa Free Zone",
+    description: "Developing the skills and talents of Libya’s people.",
+    image: "/images/InvestmentInHumanCapital.png",
+  },
+];
+
+function hoverValue(index) {
+  lastHoveredIndex.value = hoveredIndex.value;
+  hoveredIndex.value = index;
+}
+
+function clearHover() {
+  hoveredIndex.value = 0;
+}
+</script>
+
+<style scoped>
+.relative {
+  position: relative;
+}
+
+.absolute {
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
+.transition-opacity {
+  transition: opacity 0.3s ease;
+}
+</style>
--- src/views/Stories/StoriesView.vue
+++ src/views/Stories/StoriesView.vue
@@ -1,16 +1,22 @@
 <template>
   <div
-    class="grid grid-cols-12 w-full px-8 md:px-[120px]"
+    class="grid grid-cols-12 w-full px-8 md:px-[120px] mb-36"
     :style="{ height: 'calc(100vh - 107px)' }"
   >
     <HeroSectionComponent class="col-start-1 col-span-full" />
   </div>
-  <div class="grid grid-cols-12 w-full px-8 md:pr-[120px] md:px-0 h-screen">
+  <div
+    class="grid grid-cols-12 w-full px-8 md:pr-[120px] md:px-0 h-screen mb-72"
+  >
     <QuoteSectionComponent class="col-start-1 col-span-full" />
+  </div>
+  <div class="grid grid-cols-12 w-full px-8 md:px-[120px] h-screen mb-72">
+    <ProjectsSectionComponent class="col-start-1 col-span-full" />
   </div>
 </template>
 
 <script setup>
 import HeroSectionComponent from "../../components/Stories/HeroSectionComponent.vue";
 import QuoteSectionComponent from "../../components/Stories/QuoteSectionComponent.vue";
+import ProjectsSectionComponent from "../../components/Stories/ProjectsSectionComponent.vue";
 </script>
