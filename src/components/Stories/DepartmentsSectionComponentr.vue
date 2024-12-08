4055b897ce8591a7d26b4219bd7d603ad816b951
Abdullah Al-jehani <abdallahaljehani926@gmail.com>
2024-12-08 14:20:36 +0200

    completed the stories page

---
+++ src/components/Stories/DepartmentsSectionComponentr.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="text-white">
+    <h1 class="text-2xl font-semibold mb-9">Other Departments</h1>
+    <div class="grid gap-6 grid-cols-1 sm:grid-cols-12">
+      <div
+        v-for="(story, index) in stories"
+        :key="index"
+        :class="{
+          'col-span-full': true,
+          'sm:col-span-6': true,
+          [`sm:col-start-${index === 0 ? 1 : 7}`]: true,
+        }"
+      >
+        <StoryComponent
+          :image="story.image"
+          :title="story.title"
+          :description="story.description"
+          :link="story.link"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import StoryComponent from "../../helper/StoryComponent.vue";
+defineProps({
+  stories: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
--- src/views/Stories/StoriesView.vue
+++ src/views/Stories/StoriesView.vue
@@ -10,8 +10,14 @@
   >
     <QuoteSectionComponent class="col-start-1 col-span-full" />
   </div>
-  <div class="grid grid-cols-12 w-full px-8 md:px-[120px] h-screen mb-36">
+  <div class="grid grid-cols-12 w-full px-8 md:px-[120px] mb-36">
     <ProjectsSectionComponent class="col-start-1 col-span-full" />
+  </div>
+  <div class="grid grid-cols-12 w-full px-8 md:px-[120px]">
+    <DepartmentsSectionComponentr
+      :stories="stories"
+      class="col-start-1 col-span-full"
+    />
   </div>
 </template>
 
@@ -19,4 +25,21 @@
 import HeroSectionComponent from "../../components/Stories/HeroSectionComponent.vue";
 import QuoteSectionComponent from "../../components/Stories/QuoteSectionComponent.vue";
 import ProjectsSectionComponent from "../../components/Stories/ProjectsSectionComponent.vue";
+import DepartmentsSectionComponentr from "../../components/Stories/DepartmentsSectionComponentr.vue";
+const stories = [
+  {
+    image: "/images/Department1.png",
+    title: "Capacity Building",
+    description:
+      "Tatweer Research drives economic empowerment by supporting entrepreneurship through programs like Enjazi and the Tatweer Entrepreneurship Campus (TEC). These initiatives provide funding, mentorship, and training, fostering startups and creating job opportunities, while promoting Libya's economic diversification through TEC+ and the Impact Fund.",
+    link: "#",
+  },
+  {
+    image: "/images/Department2.png",
+    title: "Digital Transformation",
+    description:
+      "Tatweer enhances human capital with training programs such as the Tatweer Graduate Program, Coding for Children, and Qader. These initiatives equip Libyan youth and professionals with skills to succeed in the workforce, contributing to national growth and boosting sectors like education and public service.",
+    link: "#",
+  },
+];
 </script>
